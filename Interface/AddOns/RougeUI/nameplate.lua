f3691173904f1c6daa06e27794c407a89cf6585e
XyzKangUI <94811434+XyzKangUI@users.noreply.github.com>
2022-02-07 14:52:32 +0100

    Update nameplate.lua

--- Interface/AddOns/RougeUI/nameplate.lua
+++ Interface/AddOns/RougeUI/nameplate.lua
@@ -1,27 +1,25 @@
-SetCVar("ShowClassColorInFriendlyNameplate", 1)
+SetCVar("ShowClassColorInFriendlyNameplate", 0)
 SetCVar("ShowClassColorInNameplate", 1)
 SetCVar("nameplateMaxDistance", 41)
 
 local function AddElements(plate)
-	if (string.find(plate.unit, "nameplate") and not plate:IsForbidden()) then
-		plate.CastBar.Text:SetFont(STANDARD_TEXT_FONT, 8)
-		local sh = plate.selectionHighlight -- kang from evolvee
-		sh:SetPoint("TOPLEFT", sh:GetParent(), "TOPLEFT", 1, -1)
-		sh:SetPoint("BOTTOMRIGHT", sh:GetParent(), "BOTTOMRIGHT", -1, 1)
-		plate.name:SetFont(STANDARD_TEXT_FONT, 8)
-		plate.name:ClearAllPoints()
-     		plate.name:SetPoint('BOTTOMRIGHT', plate, 'TOPRIGHT', -6, -13)
-     		plate.name:SetJustifyH'RIGHT'
-		for _, v in pairs({plate.healthBar.border:GetRegions(), plate.CastBar.Border}) do
-                	v:SetVertexColor(0.05, 0.05, 0.05)
-		end
+	if plate:IsForbidden() then return end
+	plate.CastBar.Text:SetFont(STANDARD_TEXT_FONT, 8)
+        local sh = plate.selectionHighlight -- kang from evolvee
+       	sh:SetPoint("TOPLEFT", sh:GetParent(), "TOPLEFT", 1, -1)
+       	sh:SetPoint("BOTTOMRIGHT", sh:GetParent(), "BOTTOMRIGHT", -1, 1)
+	plate.name:SetFont(STANDARD_TEXT_FONT, 8)
+	plate.name:ClearAllPoints()
+     	plate.name:SetPoint('BOTTOMRIGHT', plate, 'TOPRIGHT', -6, -13)
+     	plate.name:SetJustifyH'RIGHT'
+	for _, v in pairs({plate.healthBar.border:GetRegions(), plate.CastBar.Border}) do
+                v:SetVertexColor(0.05, 0.05, 0.05)
 	end
 end
 
 local hookedFrames = {}
 
 local function CastBarText(plate)
-    if string.find(plate.unit, "nameplate") then
         if plate:IsForbidden() then return end
         if not hookedFrames[plate] then
             plate.CastBar:HookScript("OnShow", function()
@@ -31,7 +29,6 @@ local function CastBarText(plate)
             end)
             hookedFrames[plate] = plate
         end
-    end
 end
 
 local OnEvent = function(self, event, ...)
@@ -44,6 +41,6 @@ local OnEvent = function(self, event, ...)
     end
 end
 
-local  e = CreateFrame'Frame'
+local  e = CreateFrame("Frame")
 e:RegisterEvent("NAME_PLATE_UNIT_ADDED")
 e:SetScript('OnEvent', OnEvent)
