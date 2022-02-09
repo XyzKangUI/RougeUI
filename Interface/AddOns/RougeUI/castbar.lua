5f38a8f7734810d265d7c563ca04a4a316b31df9
XyzKangUI <94811434+XyzKangUI@users.noreply.github.com>
2022-02-09 15:49:32 +0100

    Update castbar.lua

--- Interface/AddOns/RougeUI/castbar.lua
+++ Interface/AddOns/RougeUI/castbar.lua
@@ -1,44 +1,46 @@
-local t = TargetFrameSpellBar
-t.timer = t:CreateFontString(nil, "OVERLAY")
-t.timer:SetFontObject("SystemFont_Shadow_Small")
-t.timer:SetShadowColor(0, 0, 0)
-t.timer:SetShadowOffset(1, -1)
-t.timer:SetPoint("RIGHT", t, -3, 0)
-t.update = .1
+local function modstyle()
+	local t = TargetFrameSpellBar
+	t.timer = t:CreateFontString(nil, "OVERLAY")
+	t.timer:SetFontObject("SystemFont_Shadow_Small")
+	t.timer:SetShadowColor(0, 0, 0)
+	t.timer:SetShadowOffset(1, -1)
+	t.timer:SetPoint("RIGHT", t, -3, 0)
+	t.update = .1
 
-local f = FocusFrameSpellBar
-f.timer = f:CreateFontString(nil, "OVERLAY")
-f.timer:SetFontObject("SystemFont_Shadow_Small")
-f.timer:SetShadowColor(0, 0, 0)
-f.timer:SetShadowOffset(1, -1)
-f.timer:SetPoint("RIGHT", f, -3, 0)
-f.update = .1
+	local f = FocusFrameSpellBar
+	f.timer = f:CreateFontString(nil, "OVERLAY")
+	f.timer:SetFontObject("SystemFont_Shadow_Small")
+	f.timer:SetShadowColor(0, 0, 0)
+	f.timer:SetShadowOffset(1, -1)
+	f.timer:SetPoint("RIGHT", f, -3, 0)
+	f.update = .1
 
-t.Text:SetFont(STANDARD_TEXT_FONT, 10)
-t:ClearAllPoints()
-t:SetWidth(142)
-t:SetHeight(10)
--- t.Text:SetPoint("TOP", t, 0, 4)
-t.Icon:Show()
-t.Icon:SetHeight(21)
-t.Icon:SetWidth(21)
-t.Icon:SetPoint("RIGHT", t, "LEFT", -10, 1)
-t.Text:ClearAllPoints()
-t.Text:SetPoint("TOPLEFT", t, "BOTTOMLEFT", 2, -5)
-t.Text:SetJustifyH("LEFT")
+	t.Text:SetFont(STANDARD_TEXT_FONT, 10)
+	t:ClearAllPoints()
+	t:SetWidth(142)
+	t:SetHeight(10)
+	-- t.Text:SetPoint("TOP", t, 0, 4)
+	t.Icon:Show()
+	t.Icon:SetHeight(21)
+	t.Icon:SetWidth(21)
+	t.Icon:SetPoint("RIGHT", t, "LEFT", -10, 1)
+	t.Text:ClearAllPoints()
+	t.Text:SetPoint("TOPLEFT", t, "BOTTOMLEFT", 2, -5)
+	t.Text:SetJustifyH("LEFT")
 
-f.Text:SetFont(STANDARD_TEXT_FONT, 11)
-f:ClearAllPoints()
-f:SetWidth(142)
-f:SetHeight(10)
-f.Text:SetPoint("TOP", f, 0, 4)
-f.Icon:Show()
-f.Icon:SetHeight(21)
-f.Icon:SetWidth(21)
-f.Icon:SetPoint("RIGHT", f, "LEFT", -10, 1)
-f.Text:ClearAllPoints()
-f.Text:SetPoint("TOPLEFT", f, "BOTTOMLEFT", 2, -5)
-f.Text:SetJustifyH("LEFT")
+	f.Text:SetFont(STANDARD_TEXT_FONT, 11)
+	f:ClearAllPoints()
+	f:SetWidth(142)
+	f:SetHeight(10)
+	f.Text:SetPoint("TOP", f, 0, 4)
+	f.Icon:Show()
+	f.Icon:SetHeight(21)
+	f.Icon:SetWidth(21)
+	f.Icon:SetPoint("RIGHT", f, "LEFT", -10, 1)
+	f.Text:ClearAllPoints()
+	f.Text:SetPoint("TOPLEFT", f, "BOTTOMLEFT", 2, -5)
+	f.Text:SetJustifyH("LEFT")
+end
 
 CastingBarFrame.Border:SetTexture("Interface\\CastingBar\\UI-CastingBar-Border-Small")
 CastingBarFrame.Flash:SetTexture("Interface\\CastingBar\\UI-CastingBar-Flash-Small")
@@ -66,21 +68,21 @@ local function c_OnUpdate_Hook(self, elapsed)
 	    else
 	        self.update = self.update - elapsed
 	    end
-	GradientColour(self)
 end
 
 local FR = CreateFrame("Frame")
 FR:RegisterEvent("PLAYER_LOGIN")
-FR:RegisterEvent("ADDON_LOADED")
 FR:SetScript("OnEvent", function(self, event)
-	if ((event == "PLAYER_LOGIN") or (event == "ADDON_LOADED")) then
+	if event == "PLAYER_LOGIN" then
 		if (RougeUI.CastTimer == true) then
-			f:HookScript("OnUpdate", c_OnUpdate_Hook)
-			t:HookScript("OnUpdate", c_OnUpdate_Hook)
+			modstyle()
+			FocusFrameSpellBar:HookScript("OnUpdate", function(self, elapsed) c_OnUpdate_Hook(self, elapsed) GradientColour(self) end)
+			TargetFrameSpellBar:HookScript("OnUpdate", function(self, elapsed) c_OnUpdate_Hook(self, elapsed) GradientColour(self) end)
+			CastingBarFrame:HookScript("OnUpdate", function(self) GradientColour(self) end)
 		end
 	end
-	FR:UnregisterAllEvents()
-	FR:SetScript("OnEvent", nil)
+	self:UnregisterEvent("PLAYER_LOGIN")
+	self:SetScript("OnEvent", nil)
 end);
 
     --
