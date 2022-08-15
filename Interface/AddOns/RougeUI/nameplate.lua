4c8abac2503a33ba919c54c9e2be29692685493a
XyzKangUI <94811434+XyzKangUI@users.noreply.github.com>
2022-08-15 04:33:51 +0200

    Hacks

--- Interface/AddOns/RougeUI/nameplate.lua
+++ Interface/AddOns/RougeUI/nameplate.lua
@@ -1,23 +1,22 @@
-SetCVar("ShowClassColorInFriendlyNameplate", 0)
-SetCVar("ShowClassColorInNameplate", 1)
-SetCVar("nameplateMaxDistance", 41)
+local str_split = string.split
+local UnitGUID = UnitGUID
+local hookedFrames = {}
 
 local function AddElements(plate)
 	if plate:IsForbidden() then return end
+
 	plate.CastBar.Text:SetFont(STANDARD_TEXT_FONT, 8)
-    local sh = plate.selectionHighlight -- kang from evolvee
-    sh:SetPoint("TOPLEFT", sh:GetParent(), "TOPLEFT", 1, -1)
-    sh:SetPoint("BOTTOMRIGHT", sh:GetParent(), "BOTTOMRIGHT", -1, 1)
+	local sh = plate.selectionHighlight -- kang from evolvee
+	sh:SetPoint("TOPLEFT", sh:GetParent(), "TOPLEFT", 1, -1)
+	sh:SetPoint("BOTTOMRIGHT", sh:GetParent(), "BOTTOMRIGHT", -1, 1)
 	plate.name:SetFont(STANDARD_TEXT_FONT, 8)
 	plate.name:ClearAllPoints()
-    plate.name:SetPoint('BOTTOMRIGHT', plate, 'TOPRIGHT', -6, -13)
-    plate.name:SetJustifyH'RIGHT'
+	plate.name:SetPoint('BOTTOMRIGHT', plate, 'TOPRIGHT', -6, -13)
+	plate.name:SetJustifyH'RIGHT'
 	for _, v in pairs({plate.healthBar.border:GetRegions(), plate.CastBar.Border}) do
-        v:SetVertexColor(RougeUI.Colval, RougeUI.Colval, RougeUI.Colval)
+		v:SetVertexColor(RougeUI.Colval, RougeUI.Colval, RougeUI.Colval)
 	end
 end
-
-local hookedFrames = {}
 
 local function CastBarText(plate)
         if plate:IsForbidden() then return end
@@ -31,14 +30,27 @@ local function CastBarText(plate)
         end
 end
 
-local OnEvent = function(self, event, ...)
-    if (event == "NAME_PLATE_UNIT_ADDED") then
-        local base = ...
-        local namePlateFrameBase = C_NamePlate.GetNamePlateForUnit(base, issecure());
-
-        AddElements(namePlateFrameBase.UnitFrame)
-        CastBarText(namePlateFrameBase.UnitFrame)
+-- Modification of Knall's genius pet script. Ty <3
+local function HidePlates(plate, unit)
+    local _, _, _, _, _, npcId = str_split("-", UnitGUID(unit))
+	-- snake trap
+    if npcId == "19833" or npcId == "19921" then
+        plate.UnitFrame:Hide()
+    else
+        plate.UnitFrame:Show()
     end
+end
+
+local OnEvent = function(self, event, ...)
+	if event == "NAME_PLATE_UNIT_ADDED" then
+		local base = ...
+		local namePlateFrameBase = C_NamePlate.GetNamePlateForUnit(base, issecure());
+		if not namePlateFrameBase then return end
+
+		HidePlates(namePlateFrameBase, base)
+        	AddElements(namePlateFrameBase.UnitFrame)
+        	CastBarText(namePlateFrameBase.UnitFrame)
+    	end
 end
 
 local  e = CreateFrame("Frame")
