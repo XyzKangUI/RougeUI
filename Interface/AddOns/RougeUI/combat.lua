801917451c50971681978a51b8987bce43e89fa9
XyzKangUI <94811434+XyzKangUI@users.noreply.github.com>
2022-09-04 14:50:13 +0200

    Improve combat indicator
    Better.

--- Interface/AddOns/RougeUI/combat.lua
+++ Interface/AddOns/RougeUI/combat.lua
@@ -1,7 +1,5 @@
 local UnitAffectingCombat = UnitAffectingCombat
 local pairs = pairs
-local interval = 0.05
-local lastupdate = 0
 
 local function CreateCombatIndicatorForUnit(unit, frame)
 	local ciFrame = CreateFrame("Frame", nil, frame)
@@ -10,33 +8,56 @@ local function CreateCombatIndicatorForUnit(unit, frame)
 	ciFrame.texture = ciFrame:CreateTexture(nil, "BORDER")
 	ciFrame.texture:SetAllPoints(ciFrame)
 	ciFrame.texture:SetTexture("Interface\\Icons\\ABILITY_DUALWIELD")
-    	ciFrame:Hide()
-    	ciFrame.unit = unit
-    	return ciFrame
+	ciFrame:Hide()
+	ciFrame.unit = unit
+	return ciFrame
 end
-
-local function FrameOnUpdate(self, elapsed)
-	if (RougeUI.CombatIndicator == false) then
-		self:SetScript("OnUpdate", nil)
-		return
-	end
-	lastupdate = lastupdate + elapsed
-	if lastupdate >= interval then
-		lastupdate = 0
-		for _,ciFrame in pairs(self.ciFrames) do
-			if UnitAffectingCombat(ciFrame.unit) then 
-				ciFrame:Show() 
-			else
-				ciFrame:Hide() 
-			end 
-		end
-	end
-end
-
 
 local ciCore = CreateFrame("Frame")
 ciCore.ciFrames = {}
-ciCore:SetScript("OnUpdate", FrameOnUpdate)
+ciCore:RegisterEvent("UNIT_FLAGS")
+ciCore:RegisterEvent("PLAYER_TARGET_CHANGED")
+ciCore:RegisterEvent("PLAYER_FOCUS_CHANGED")
+ciCore:RegisterEvent("ADDON_LOADED")
+
+function ciCore:UNIT_FLAGS(unitTarget)
+	if not (unitTarget == "target" or unitTarget == "focus") then return end
+
+	for _,ciFrame in pairs(self.ciFrames) do
+		if UnitAffectingCombat(ciFrame.unit) then 
+			ciFrame:Show() 
+		else
+			ciFrame:Hide()
+		end 
+	end
+end
+
+function ciCore:PLAYER_TARGET_CHANGED()
+	if UnitAffectingCombat("target") then 
+		self.ciFrames["target"]:Show() 
+	else
+		self.ciFrames["target"]:Hide() 
+	end
+end
+
+function ciCore:PLAYER_FOCUS_CHANGED()
+	if UnitAffectingCombat("focus") then 
+		self.ciFrames["focus"]:Show() 
+	else
+		self.ciFrames["focus"]:Hide() 
+	end
+end
+
+ciCore:SetScript("OnEvent", function(self, event, ...)
+	if event == "ADDON_LOADED" and not RougeUI.CombatIndicator then
+		self:UnregisterAllEvents()
+		self:SetScript("OnEvent", nil)
+	elseif event ~= "ADDON_LOADED" then
+		self[event](self, ...)
+		self:UnregisterEvent("ADDON_LOADED")
+	end
+end)
+
 
 ciCore.ciFrames["target"] = CreateCombatIndicatorForUnit("target", TargetFrame)
 ciCore.ciFrames["focus"] = CreateCombatIndicatorForUnit("focus", FocusFrame)
