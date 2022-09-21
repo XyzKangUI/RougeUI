aba1f553631523e0380a30a17e16a9f17de93758
XyzKangUI <94811434+XyzKangUI@users.noreply.github.com>
2022-09-21 02:50:02 +0200

    Update
    Lil bit of blizz, lil bit of knall, lil bit of everything makes the perfect concoction

---
+++ Interface/AddOns/RougeUI/sappable.lua
@@ -0,0 +1,84 @@
+local plates = {}
+
+local sappableLocales = {
+	'Humanoid', 'Humanoide', 'Humanoïde', 'Umanoide', 'Humanoide', 'Гуманоид', '인간형', '인간형', '人型生物',
+	'Demon', 'Dämon', 'Demonio', 'Demonio', 'Démon', 'Demone', 'Demônio', 'Демон', '악마', '恶魔', '惡魔',
+	'Beast', 'Wildtier', 'Bestia', 'Bestia', 'Bête', 'Bestia', 'Fera', 'Животное', '야수', '野兽', '野獸',
+	'Dragonkin', 'Drachkin', 'Dragon', 'Dragón', 'Draconien', 'Dragoide', 'Dracônico', 'Дракон', '용족', '龙类', '龍類'
+};
+
+local function CreatureCanBeSapped(unit)
+	creatureType = UnitCreatureType(unit)
+	for k,value in pairs(sappableLocales) do 
+		if (value == creatureType) then
+                	return true;
+		end
+	end
+	
+	return false;
+end;
+
+local function IsSappable(unit)
+    return (not UnitAffectingCombat(unit) and CreatureCanBeSapped(unit))
+end
+
+local function CreateSapIcon(unit, unitGUID)
+    local plate = C_NamePlate.GetNamePlateForUnit(unit)
+
+    if not plate.indicator then
+        plate.indicator = plate:CreateTexture(nil, "OVERLAY")
+        plate.indicator:SetTexture("Interface\\Icons\\ABILITY_SAP");
+        plate.indicator:SetSize(20, 20)
+        plate.indicator:SetPoint("LEFT", -20, -10)
+        plate.indicator:Hide()
+    end
+
+    plate.indicator.unit = unit
+    if IsSappable(unit) then
+        plate.indicator:Show()
+    end
+    plates[unitGUID] = plate
+end
+
+local function UpdateIndicator(unit, unitGUID)
+    if plates[unitGUID] then
+        if IsSappable(unit) then
+            plates[unitGUID].indicator:Show()
+        else
+            plates[unitGUID].indicator:Hide()
+        end
+    end
+end
+
+local function Remove(unitGUID)
+    if plates[unitGUID] then
+        plates[unitGUID].indicator:Hide()
+    end
+    plates[unitGUID] = nil
+end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("NAME_PLATE_UNIT_ADDED")
+frame:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
+frame:RegisterEvent("UNIT_FLAGS")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function(self, event, ...)
+    if not RougeUI.sappable then
+        self:UnregisterAllEvents()
+	  self:SetScript("OnEvent", nil)
+    end
+
+    if event == "PLAYER_ENTERING_WORLD" then
+        plates = {}
+    else
+        local unit = ...
+        local unitGUID = UnitGUID(unit)
+        if event == "NAME_PLATE_UNIT_ADDED" then
+            CreateSapIcon(unit, unitGUID)
+        elseif event == "NAME_PLATE_UNIT_REMOVED" then
+            Remove(unitGUID)
+        elseif event == "UNIT_FLAGS" then
+            UpdateIndicator(unit, unitGUID)
+        end
+    end
+end)
--- Interface/AddOns/RougeUI/smooth.lua
+++ Interface/AddOns/RougeUI/smooth.lua
@@ -1,24 +1,32 @@
-local smoothing = {}
+---------------------------------------------------------------------
+-- Smooth animations
+
 local floor = math.floor
-local mabs = math.abs
-local min, max = math.min, math.max
+local activeObjects = {}
+local handledObjects = {}
+local TARGET_FPS = 60
+local AMOUNT = .33	
+local abs = math.abs
 local UnitGUID = UnitGUID
-local ONUPDATE_INTERVAL = 0.01
-local TimeSinceLastUpdate = 0
 
 local barstosmooth = {
-	PlayerFrameHealthBar = "player",
-	PlayerFrameManaBar = "player",
-	PetFrameHealthBar = "pet",
-	PetFrameManaBar = "pet",
-	TargetFrameHealthBar = "target",
-	TargetFrameManaBar = "target",
-	FocusFrameHealthBar = "focus",
-	FocusFrameManaBar = "focus",
-}
-
-local smoothframe = CreateFrame("Frame")
-smoothframe:RegisterEvent("ADDON_LOADED")
+   PlayerFrameHealthBar = "player",
+   PlayerFrameManaBar = "player",
+   TargetFrameHealthBar = "target",
+   TargetFrameManaBar = "target",
+   PetFrameHealthBar = "pet",
+   PetFrameManaBar = "pet",
+   FocusFrameHealthBar = "focus",
+   FocusFrameManaBar = "focus",
+   PartyMemberFrame1HealthBar = "party1",
+   PartyMemberFrame1ManaBar = "party1",
+   PartyMemberFrame2HealthBar = "party2",
+   PartyMemberFrame2ManaBar = "party2",
+   PartyMemberFrame3HealthBar = "party3",
+   PartyMemberFrame3ManaBar = "party3",
+   PartyMemberFrame4HealthBar = "party4",
+   PartyMemberFrame4ManaBar = "party4",
+    }
 
 local function isPlate(frame)
 	local name = frame:GetName()
@@ -29,93 +37,138 @@ local function isPlate(frame)
 	return false
 end
 
-local function AnimationTick()
-	local limit = .33
-	for bar, value in pairs(smoothing) do
-		local cur = bar:GetValue()
-		local new = cur + min((value - cur) /3, max(value - cur, limit))
+local function clamp(v, min, max)
+	min = min or 0
+	max = max or 1
 
-		if new ~= new then
-			new = value
-		end
+	if v > max then
+		return max
+	elseif v < min then
+		return min
+	end
 
-		bar:SetValue_(floor(new))
-		if cur == value or mabs(new - value) < 2 then
-			bar:SetValue_(value)
-			smoothing[bar] = nil
+	return v
+end
+
+local function isCloseEnough(new, target, range)
+	if range > 0 then
+		return abs((new - target) / range) <= 0.001
+	end
+
+	return true
+end
+
+local smoothframe = CreateFrame("Frame")
+local function AnimationTick(_, elapsed)
+	for unitFrame, info in next, activeObjects do
+		local new = Lerp(unitFrame._value, info.currentHealth, clamp(AMOUNT * elapsed * TARGET_FPS))
+		if info.changedGUID or isCloseEnough(new, info.currentHealth, unitFrame._max - unitFrame._min) then
+			new = info.currentHealth
+			activeObjects[unitFrame] = nil
 		end
+		unitFrame:SetValue_(floor(new))
+		unitFrame._value = new
 	end
 end
 
-local function SmoothSetValue(self, value)
+local function bar_SetSmoothedValue(self, value)
 	self.finalValue = value
-	if self.unitType then
-		local guid = UnitGUID(self.unitType)
-		if (value == self:GetValue() or (not guid or guid ~= self.lastGuid)) then
-			smoothing[self] = nil
-			self:SetValue_(value)
-		else
-			smoothing[self] = value
-		end
-		self.lastGuid = guid
-	else
-		local _, max = self:GetMinMaxValues()
-		if (value == self:GetValue() or (self._max and self._max ~= max)) then
-			smoothing[self] = nil
-			self:SetValue_(value)
-		else
-			smoothing[self] = value
-		end
-		self._max = max
+
+	value = tonumber(value)
+	self._value = self:GetValue()
+	if not activeObjects[self] then
+		activeObjects[self] = {}
 	end
+	if self.unit then
+		local guid = UnitGUID(self.unit)
+		if guid ~= self.guid then
+			activeObjects[self].changedGUID = true
+		end
+		self.guid = guid
+	end
+
+	activeObjects[self].currentHealth = clamp(value, self._min, self._max)
 end
+
+local function bar_SetSmoothedMinMaxValues(self, min, max)
+	min, max = tonumber(min), tonumber(max)
+
+	self:SetMinMaxValues_(min, max)
+
+	if self._max and self._max ~= max then
+		local ratio = 1
+		if max ~= 0 and self._max and self._max ~= 0 then
+			ratio = max / (self._max or max)
+		end
+
+		local currentHealth = activeObjects[self] and activeObjects[self].currentHealth
+		if currentHealth then
+			activeObjects[self].currentHealth = currentHealth * ratio
+		end
+
+		local cur = self._value
+		if cur then
+			self:SetValue_(cur * ratio)
+			self._value = cur * ratio
+		end
+	end
+
+	self._min = min
+	self._max = max
+end
+
 
 local function SmoothBar(bar)
+	bar._min, bar._max = bar:GetMinMaxValues()
+	bar._value = bar:GetValue()
+
 	if not bar.SetValue_ then
 		bar.SetValue_ = bar.SetValue
-		bar.SetValue = SmoothSetValue
+		bar.SetValue = bar_SetSmoothedValue
 	end
+	if not bar.SetMinMaxValues_ then
+		bar.SetMinMaxValues_ = bar.SetMinMaxValues
+		bar.SetMinMaxValues = bar_SetSmoothedMinMaxValues
+	end
+
+	handledObjects[bar] = true
 end
 
 local function onUpdate(self, elapsed)
-	TimeSinceLastUpdate = TimeSinceLastUpdate + elapsed
-	if TimeSinceLastUpdate >= ONUPDATE_INTERVAL then
-		TimeSinceLastUpdate = 0
-
-		local frames = {WorldFrame:GetChildren()}
-		for _, plate in ipairs(frames) do
-			if not plate:IsForbidden() and isPlate(plate) and C_NamePlate.GetNamePlates() and plate:IsVisible() then
-				local v = plate:GetChildren()
-				if  v.healthBar then
-					SmoothBar(v.healthBar)
-				end
+	local frames = {WorldFrame:GetChildren()}
+	for _, plate in ipairs(frames) do
+		if not plate:IsForbidden() and isPlate(plate) and C_NamePlate.GetNamePlates() and plate:IsVisible() then
+			local v = plate:GetChildren()
+			if  v.healthBar then
+				SmoothBar(v.healthBar)
 			end
 		end
-
-		AnimationTick()
 	end
+	AnimationTick(_, elapsed)
 end
 
 local function init()
-	for k,v in pairs (barstosmooth) do
-		if _G[k] then
-			SmoothBar(_G[k])
-			_G[k]:HookScript("OnHide", function()
-				_G[k].lastGuid = nil;
-				_G[k].max_ = nil
-			end)
+	for k, v in pairs(barstosmooth) do
+    		if _G[k] then
+        		SmoothBar(_G[k])
+        		_G[k]:HookScript("OnHide", function()
+            		_G[k].lastGuid = nil;
+            		_G[k].min_ = nil
+            		_G[k].max_ = nil
+       	 	end)
 			if v ~= "" then
-				_G[k].unitType = v
+				_G[k].unit = v
 			end
-		end
+   	 	end
 	end
 end
 
+smoothframe:RegisterEvent("ADDON_LOADED")
 smoothframe:SetScript("OnEvent", function(self, event)
 	if event == "ADDON_LOADED" and RougeUI.smooth == true then
-		smoothframe:HookScript("OnUpdate", onUpdate)
+		smoothframe:SetScript("OnUpdate", onUpdate)
 		init()
 	end
 	self:UnregisterEvent("ADDON_LOADED")
 	self:SetScript("OnEvent", nil)
-end);
+end)
