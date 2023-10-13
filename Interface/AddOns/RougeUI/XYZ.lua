429baf228e92e2552da489465e078f99ba04cbe5
XyzKangUI <94811434+XyzKangUI@users.noreply.github.com>
2023-10-13 18:24:33 +0200

    3.40

--- Interface/AddOns/RougeUI/XYZ.lua
+++ Interface/AddOns/RougeUI/XYZ.lua
@@ -9,20 +9,24 @@ local SecureHandlerUnwrapScript = _G.SecureHandlerUnwrapScript
 local tonumber = _G.tonumber
 local frame = CreateFrame("Frame")
 
-local function WAHK(button)
+local function WAHK(button, ok)
     if not button then
         return
     end
 
-    local clickButton
     local btn = _G[button]
+    if not btn then
+        return
+    end
 
+    local clickButton, id
+    local clk = tostring(button)
     if button:match("BT4Button") then
         clickButton = ("CLICK %s:LeftButton"):format(button)
     elseif button:match("DominosActionButton") then
         clickButton = ("CLICK %s:HOTKEY"):format(button)
     else
-        local id = tonumber(button:match("(%d+)"))
+        id = tonumber(button:match("(%d+)"))
         local actionButtonType = btn.buttonType
         local buttonType = actionButtonType and (actionButtonType .. id) or ("ACTIONBUTTON%d"):format(id)
         clickButton = buttonType or ("CLICK " .. button .. ":LeftButton")
@@ -30,15 +34,30 @@ local function WAHK(button)
 
     local key = GetBindingKey(clickButton)
 
-    if btn then
-        btn:RegisterForClicks("AnyDown", "AnyUp")
-        if dominos then
-            SecureHandlerUnwrapScript(btn, "OnClick")
-        end
+    if btn and dominos then
+        SecureHandlerUnwrapScript(btn, "OnClick")
     end
 
-    if key and not bartender then
-        SetOverrideBindingClick(btn, true, key, btn:GetName())
+    if key and btn then
+        if ok then
+            local wahk = CreateFrame("Button", "WAHK" .. button, nil, "SecureActionButtonTemplate")
+            wahk:RegisterForClicks("AnyDown", "AnyUp")
+            wahk:SetAttribute("type", "macro")
+            local onclick = string.format([[ local id = tonumber(self:GetName():match("(%d+)")) if down then if HasVehicleActionBar() then self:SetAttribute("macrotext", "/click OverrideActionBarButton" .. id) else self:SetAttribute("macrotext", "/click ActionButton" .. id) end else if HasVehicleActionBar() then self:SetAttribute("macrotext", "/click OverrideActionBarButton" .. id) else self:SetAttribute("macrotext", "/click ActionButton" .. id) end end]], id, id, id)
+            SecureHandlerWrapScript(wahk, "OnClick", wahk, onclick)
+            SetOverrideBindingClick(wahk, true, key, wahk:GetName())
+            wahk:SetScript("OnMouseDown", function() if HasVehicleActionBar() then _G["OverrideActionBarButton"..id]:SetButtonState("PUSHED") else btn:SetButtonState("PUSHED") end end)
+            wahk:SetScript("OnMouseUp", function() if HasVehicleActionBar() then _G["OverrideActionBarButton"..id]:SetButtonState("NORMAL") else btn:SetButtonState("NORMAL") end end)
+        else
+            btn:RegisterForClicks("AnyDown", "AnyUp")
+            local onclick = ([[ if down then
+        self:SetAttribute("macrotext", "/click clk") else self:SetAttribute("macrotext", "/click clk") end
+    ]]):gsub("clk", clk), nil
+            SecureHandlerWrapScript(btn, "OnClick", btn, onclick)
+            if not bartender then
+                SetOverrideBindingClick(btn, true, key, btn:GetName())
+            end
+        end
     end
 end
 
@@ -48,16 +67,8 @@ local function UpdateBinds()
         return
     end
 
-    if OverrideActionBar and OverrideActionBar:IsShown() then
-        for i = 1, 6 do
-            local ob = _G["ActionButton" .. i];
-            ClearOverrideBindings(ob)
-        end
-        return
-    end
-
     for i = 1, 12 do
-        WAHK("ActionButton" .. i)
+        WAHK("ActionButton" .. i, true)
         WAHK("MultiBarBottomRightButton" .. i)
         WAHK("MultiBarBottomLeftButton" .. i)
         WAHK("MultiBarRightButton" .. i)
@@ -77,8 +88,6 @@ end
 
 frame:RegisterEvent("UPDATE_BINDINGS")
 frame:RegisterEvent("PLAYER_LOGIN")
-frame:RegisterUnitEvent("UNIT_ENTERED_VEHICLE", "player")
-frame:RegisterUnitEvent("UNIT_EXITED_VEHICLE", "player")
 frame:SetScript("OnEvent", function(self, event, ...)
     if not RougeUI.db.KeyEcho then
         -- stop copying my work, use the addon.
